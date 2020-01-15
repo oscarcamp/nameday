cdb324101a119f0c9d091c3a89ee6c2faf67320d
oscarcamp <33449861+oscarcamp@users.noreply.github.com>
2020-01-15 09:07:16 +0100

    Can insert name and country and get info about which day, month and other names that has nameday same day

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,12 +1,14 @@
 
 const submit = document.querySelector("#submit");
-const nameEl = document.querySelector("#name")
+const nameEl = document.querySelector("#name");
+const countryEL = document.querySelector("#country");
 
 submit.addEventListener('click', async function () {
 
     const input1 = nameEl.value;
+    const input2 = countryEL.value;
 
-    const response = await fetch(`https://api.abalin.net/getdate?name=${input1}&country=se`);
+    const response = await fetch(`https://api.abalin.net/getdate?name=${input1}&country=${input2}`);
     const info = await response.json()
     console.log(info);
 
